21de00740ad098c2fdbc8ea263418988891eff58
masaki <masakisato.japan@gmail.com>
2024-09-27 13:43:33 +0900

    add L10n config
    added arb files once manually

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'l10n/l10n.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +12,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      onGenerateTitle: (context) => L10n.of(context).exampleTitle,
+      localizationsDelegates: L10n.localizationsDelegates,
+      supportedLocales: L10n.supportedLocales,
+      localeListResolutionCallback: (locales, supportedLocales) {
+        final locale = basicLocaleListResolution(locales, supportedLocales);
+        Intl.defaultLocale = locale.toString();
+        return locale;
+      },
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
